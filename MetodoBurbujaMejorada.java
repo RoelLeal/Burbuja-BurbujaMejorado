552907c51d5c1324ba160a56a7d4a0e59424cd3c
Roel Leal <50306254+RoelLeal@users.noreply.github.com>
2022-09-26 11:51:11 -0500

    Add files via upload

---
+++ MetodoBurbujaMejorada.java
@@ -0,0 +1,49 @@
+package com.mycompany.metodoburbujamejorada;
+import java.util.Scanner;
+public class MetodoBurbujaMejorada {
+    //ANGEL ROEL LEAL ALVARADO ISC 21380638
+    static int[] vek = new int[5];
+    static int temp = 0, cont = 0;
+    static boolean marcador = false;
+    static Scanner entrada = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        llenadoArreglo();
+        ordenamientoArreglo();
+        impresionArregloCompleto();
+    }
+    public static void llenadoArreglo(){
+        for(int i = 0; i < 5; i++){
+            System.out.print("Ingresa el valor en ["+i+"]: ");
+            vek[i] = entrada.nextInt();
+        }
+    }
+    public static void ordenamientoArreglo(){
+        for(int i = 0; i < 4 && marcador == false; i++){
+            marcador = true;
+            for(int j = 0; j < vek.length-i-1; j++){
+                cont++;
+                for(int k = 0; k < 5; k++){
+                    System.out.print(vek[k]+" | ");
+                }
+                if(vek[j] > vek[j+1]){
+                    marcador = false;
+                    temp = vek[j+1];
+                    vek[j+1] = vek[j];
+                    vek[j] = temp;
+                    System.out.println("El valor: "+vek[j]+" sustituyó el valor: "+vek[j+1]+" cambiado de indice: "+(j+1)+" a indice: "+j);
+                }else{
+                    System.out.println("No hubo cambios en el arreglo");
+                }
+            }
+        }
+    }
+    public static void impresionArregloCompleto(){
+        System.out.println();
+        System.out.println("Numero de interaciones: "+cont);
+        System.out.println("El arreglo ordenado nos quedó: ");
+        for(int i = 0; i < 5; i++){
+            System.out.print(vek[i]+" - ");
+        }
+    }
+}
